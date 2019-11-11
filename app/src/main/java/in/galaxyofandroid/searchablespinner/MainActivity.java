1eb85fb5fb57e63606c1b168141c277c64984d4a
Igor <igorka.mishcenko@gmail.com>
2019-11-12 00:14:47 +0200

    added Hide Search property

--- app/src/main/java/in/galaxyofandroid/searchablespinner/MainActivity.java
+++ app/src/main/java/in/galaxyofandroid/searchablespinner/MainActivity.java
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
         spinnerDialog.setCancellable(true);
         spinnerDialog.setShowKeyboard(false);
 
-
         spinnerDialog.bindOnSpinerListener(new OnSpinerItemClick() {
             @Override
             public void onClick(String item, int position) {
